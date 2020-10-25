df0c1dbfea6e98b34d38991526a435ea165b6b96
Samet İrkören <sametirkoren@gmail.com>
2020-10-25 13:20:28 +0300

    Dependency Injection

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+
+        void Add(Product product);
+
+        void Delete(Product product);
+
+        void Update(Product product);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
+        public List<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+
+        }
+    }
+}
