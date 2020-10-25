07dada517a8a338d1b024b76d13cb2439308472a
Samet İrkören <sametirkoren@gmail.com>
2020-10-25 11:41:14 +0300

    DbContext

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Northwind;Trusted_Connection=true");    
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
