4807277435354eef1775dd025ff13f5753fbc6ed
Samet İrkören <sametirkoren@gmail.com>
2020-11-30 16:30:02 +0300

    AOP Transaction

---
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,32 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                // başarılıysa
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                // başarılı değilse yapılan işlemleri geri al
+                catch
+                {
+                    transactionScope.Dispose();
+                    
+                    throw;
+                }
+            }
+        }
+    }
+}
