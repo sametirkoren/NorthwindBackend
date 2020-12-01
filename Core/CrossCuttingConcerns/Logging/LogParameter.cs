23b39881e621740c48a15452fb2198491feeca58
Samet İrkören <sametirkoren@gmail.com>
2020-12-01 17:00:20 +0300

    Log config file

---
+++ Core/CrossCuttingConcerns/Logging/LogParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Logging
+{
+    public class LogParameter
+    {
+        public string Name { get; set; } // Product örneğin
+
+        public object Value { get; set; } // Product Name ' i Araba , Id si 1 gibi düşünebilirsiniz.
+
+        public string Type { get; set; }
+    }
+}
